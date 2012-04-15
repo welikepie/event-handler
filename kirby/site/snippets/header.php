ee2b4dfb8bb6b22d693cf1921d6edd21d45b07e1
Mikołaj Banasik <d1sover@gmail.com>
2012-04-15 15:52:50 +0100

    Added Google tracking.

--- kirby/site/snippets/header.php
+++ kirby/site/snippets/header.php
@@ -17,6 +17,19 @@
 	<script type="text/javascript" src="/assets/scripts/details.js"></script>
 	<script type="text/javascript" src="http://use.typekit.com/enx4ueb.js"></script>
 	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-30903988-1']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
 </head>
 
 <body>
