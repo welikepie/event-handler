44015095c70e288c172075dc62b7ee6506b3cfc5
Alexander Michael Roche <alex@amroche.co.uk>
2012-08-02 22:50:55 +0100

    Calendar and Agenda support from multiple sources.

--- kirby/site/templates/calendar.php
+++ kirby/site/templates/calendar.php
@@ -9,30 +9,99 @@
 <?php
 snippet('page_sort');
 $count = 0;
+
+
 $events = $pages -> find('events');
 $events = $events -> children() -> visible();
-page_sort($events, array('date_sort', 'priority_sort'));
-echo("<table><thead><th>Date</th><th>Event</th><th>Location</th></thead>");
-echo("<tbody>");
-//sort shit by date
-foreach ($events as $event) {
-	$count += 1;
-	if ($count >= 20) {
-		break;
+$events = $events -> sortBy('date', 'asc');
+
+$foreignevents = $pages -> find('foreignevents');
+$foreignevents = $foreignevents -> children() -> visible();
+$foreignevents = $foreignevents -> sortBy('date', 'asc');
+
+$eventArray = array();
+
+foreach ($events as $event){
+$eventArray[] = $event;	
+}
+foreach ($foreignevents as $foreignevent){
+$eventArray[] = $foreignevent;	
+}
+
+for($j=1; $j < count($eventArray); $j++) 
+	{
+        $tmp = $eventArray[$j];
+        $i = $j;
+    	    while(($i >= 0) && ($eventArray[$i-1]->date() > $tmp->date())) 
+    	    	{
+            		$eventArray[$i] = $eventArray[$i-1];
+            		$i--;
+        		}
+        $eventArray[$i] = $tmp;
+	}?>
+
+
+
+
+<?php 
+$currentdate = getdate(); 
+$monthvar = $currentdate['month'];
+$mdayvar = $currentdate['mday'];
+$wdayvar = $currentdate['wday'];
+?>
+<h1><?php echo ($monthvar); ?></h1>
+<div id="callendar">
+<div class = "calendarheader"><span class = "cal c1">Monday</span><span class = "cal c2">Tuesday</span><span class = "cal c3">Wednesday</span>
+	<span class = "cal c4">Thursday</span><span class = "cal c5">Friday</span><span class = "cal c6">Saturday</span><span class = "cal c7">Sunday</span></div>
+	<!--check class names and make for loop happen, because you're a bawss.-->	
+<?php
+
+$iMax = date("t");
+for($i = 1; $i < $iMax+1; $i++){
+	echo("<span>");
+	echo("||".(($i+($mdayvar%$wdayvar))%7).",".$i."||");
+	foreach($eventArray as $event){
+		if($event->date()!=0)
+			{
+				$month = date("F",$event-> date());
+					if($monthvar == $month)
+						{
+						
+							if($i == date("j",$event->date()))
+								{
+									echo(date("jS",$event->date()).", ".$event->title());
+								}
+						}
+				
+			}
 	}
-	$event_date = $event -> date();
-	if ($event_date != 0) {
-		$current_date = mktime(0, 0, 0, date('n'), date('j'), date('Y'));
-		//if($current_date < $event_date){
-		$event_date = date("jS F Y ", $event_date);
-		echo("<tr><td>" . $event_date . "</td><td>" . $event -> title() . "</td><td>" . $event -> where() . "</td></tr>");
-		//}
+	echo("</span>");
+	if((($i+($mdayvar%$wdayvar))%7)==0){
+		echo("<br>");
 	}
 }
-echo("</tbody></table>");
 ?>
-	
+<br><br>
 </div>
+
+
+
+	
+<div id = "acalendar">
+<h1>Agenda</h1><br><span class = "date" style = " width:"100px"; ">Date</span><span class = "event" style = " width:"100px";">Event</span>
+<span class = "location" style = "width:"100px";">Location</span>
+
+<?php
+	foreach($eventArray as $event){
+		if($event->date() != 0){
+		$event_date = $event->date();
+	echo("<li class = \"acalendar\"><span class = \"date\">" . date("jS F Y ", $event_date) . "</span><span class = \"event\">" . $event -> title() . "</span><span class=\"location\">" . $event -> where() . "</span>");
+		}
+	}
+?>
+</div>
+</div>
+
 </section>
 
 <?php snippet('footer') ?>
