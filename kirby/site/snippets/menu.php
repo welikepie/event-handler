e05f96506224824a9bad2a07ccc5fdafba6d0dc8
Alexander Michael Roche <alex@amroche.co.uk>
2012-08-09 17:30:33 +0100

    Styling, redirect page and 404 page
    - Styling of speaker submission form.
    - Adding a 404 page.
    - Adding a page for a form submit to redirect to.
    - Added a footer specific for the form page.
    - Shuffled menu items a bit.
    - Started bugfixing in calendar.

--- kirby/site/snippets/menu.php
+++ kirby/site/snippets/menu.php
@@ -2,8 +2,8 @@
   <a href="<?php echo url('') ?>">Home</a>
   <a href="<?php echo url('podcast') ?>">. Podcast</a>
   <a href="<?php echo url('jobs') ?>">. Jobs</a>
+  <a href="<?php echo url('speak') ?>">. Speak</a>
   <a href="<?php echo url('about-us') ?>">. About</a>
   <a href="<?php echo url('workshops') ?>">. Workshops</a>
-  <a href="<?php echo url('speak') ?>">. Speak</a>
   <a href="<?php echo url('calendar')?>">. Calendar</a>
 </nav>
