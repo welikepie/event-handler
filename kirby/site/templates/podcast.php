f6bca374d7108c4d85324d9b974411e3bfcf5b4d
Mikołaj Banasik <d1sover@gmail.com>
2012-08-10 10:03:43 +0100

    Restyled podcasts page for the inclusion of videos.

--- kirby/site/templates/podcast.php
+++ kirby/site/templates/podcast.php
@@ -15,7 +15,40 @@
 				
 					?><div class="profile">
 						<h2><?php echo html($name); ?></h2>
-						<div><?php echo kirbytext($profile['Text']); ?></div>
+						<!-- <?php var_dump($profile['Description']); ?>-->
+						<div class="description"><?php echo kirbytext($profile['Description']); ?></div>
+						<div class="lists">
+						<?php
+						
+							// Podcasts list
+							if (count($profile['Podcasts'])) {
+								echo('<!-- '); var_dump($profile['Podcasts']); echo(' -->');
+								echo('<ul class="podcasts">');
+								foreach ($profile['Podcasts'] as $title => $link) {
+									echo('<li><a href="' . $link . '">' . $title . '</a></li>');
+								}
+								echo('</ul>');
+							}
+							
+							// Videos list
+							if (count($profile['Videos'])) {
+								echo('<ul class="videos">');
+								foreach ($profile['Videos'] as $title => $link) {
+								
+									// Vimeo embed
+									$temp = array(); 
+									if (preg_match('/^http:\/\/(?:www\.)?vimeo.com\/([0-9]+)/i', $link, $temp)) {
+										$link = 'http://player.vimeo.com/video/' . $temp[1];
+									}
+									
+									echo('<li><iframe src="' . $link . '" width="480" height="358" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe><p>' . $title . '</p></li>');
+									
+								}
+								echo('</ul>');
+							}
+						
+						?>
+						</div>
 					</div><?php
 				
 				} unset($profile); unset($profiles);
