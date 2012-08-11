b052e891ebb24947c11be7d5be782ddc751ba44a
Alexander Michael Roche <alex@amroche.co.uk>
2012-08-11 18:11:23 +0100

    Workshops form verification - form verification error message now disappears upon correction and submission.

--- kirby/site/snippets/workshopFooter.php
+++ kirby/site/snippets/workshopFooter.php
@@ -20,9 +20,7 @@ function formsubmit() {
 	
 
 	if (x==null || x=="")
-	  {
-	  
-	  
+	  { 
 		document.getElementById("emailMissing").innerHTML = 
  "<div class = \"boxLeftEmpty bottomShove\">"+
 			"<div class = \"text\"><strong>Warning!</strong>  Oh dear, it seems you forgot to tell us your email address. Please type that in and submit the form again. "+		
@@ -30,6 +28,15 @@ function formsubmit() {
 				
 	  return false;
 	  }
+
+	if (x!=null || x!="")
+	  { 
+		document.getElementById("emailMissing").innerHTML = 
+ "" ;
+				
+	  return true;
+	  }
+
 	
 return true;
 }
