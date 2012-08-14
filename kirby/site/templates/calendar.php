037817593b0954db2dcf14309a4fda867cb0de56
Alexander Michael Roche <alex@amroche.co.uk>
2012-08-14 18:53:43 +0100

    stylings and preliminary work on making the calendar change month.

--- kirby/site/templates/calendar.php
+++ kirby/site/templates/calendar.php
@@ -37,13 +37,61 @@ $eventArray = $eventArray -> sortBy('date', 'asc');
  * will put the day of the calendar into the string for comparing with event's date.
  * $offset is used to offset the calendar fields so that they align with weekdays.
  */
+
+if((!isset($_POST['nextMonth']))&&(!isset($_POST['prevMonth']))){
 $today = getdate();
+$currentMonth = date(n);
+$currentYear = date(Y);
+}
 $comparison = date('Y-m-%02\d');
 $offset = date("N", strtotime(date('m') . '/01/' . date('Y') . ' 00:00:00'));
+
+if(isset($_POST['nextMonth'])){
+$currentMonth = $_POST['monthUp'];
+$currentYear = $_POST['Jahr'];
+	if($currentMonth > 12){
+		$currentYear = $_POST['yearUp'];
+		$currentMonth = 1;
+	}
+
+}
+
+if(isset($_POST['prevMonth'])){
+$currentMonth = $_POST['monthDown'];
+$currentYear = $_POST['Jahr'];
+if($currentMonth < 1){
+		$currentYear = $_POST['yearDown'];
+		$currentMonth = 12;
+	}
+}
+
+$trueToday = false;
+$today = getdate();
+if($currentMonth == date('n') && $currentYear == date('Y') ){
+$trueToday = true;
+}
+else{
+$trueToday = false;
+}
 			?>
 			<!-- Calendar of events -->
 			<div class="fullcal">
-				<h1><?php echo($today['month']); ?></h1>
+				<h1><?php echo(date( 'F', mktime(0, 0, 0, $currentMonth)).", ".$currentYear); ?></h1>
+				<?php echo ($currentMonth.",".$currentYear);
+				 	 echo(date('n').",".date('Y'));
+				 	 echo($trueToday);?>
+<form action = "calendar.php" method="post">
+<input type = "number" class = "hidden" name="monthUp" value = "<?php echo($currentMonth+1)?>">
+<input type = "number" class = "hidden" name="Jahr" value = "<?php echo($currentYear)?>">
+<input type = "number" class = "hidden" name="yearUp" value = "<?php echo($currentYear+1)?>">
+<input type="submit" name="nextMonth" value="nextMonth" />
+</form>
+<form action = "calendar.php" method="post">
+<input type = "number" class = "hidden" name="monthDown" value = "<?php echo($currentMonth-1)?>">
+<input type = "number" class = "hidden" name="Jahr" value = "<?php echo($currentYear)?>">
+<input type = "number" class = "hidden" name="yearDown" value = "<?php echo($currentYear-1)?>">
+<input type="submit" name="prevMonth" value="prevMonth" />
+</form>
 				<div class="calendar">
 					<div class="calendarheader">
 						<div class="calHead right">Monday</div>
@@ -53,8 +101,7 @@ $offset = date("N", strtotime(date('m') . '/01/' . date('Y') . ' 00:00:00'));
 						<div class="calHead both">Friday</div>
 						<div class="calHead both">Saturday</div>
 						<div class="calHead left">Sunday</div>
-					</div>
-
+					</div>
 					<?php
 
 					// Iterate over all days of the current month
@@ -65,26 +112,34 @@ $offset = date("N", strtotime(date('m') . '/01/' . date('Y') . ' 00:00:00'));
 
 						// Add class for the indicator of current day
 						if ($i === intval($today['mday'], 10)) {
+						 if($trueToday == 1){
 							$classes[] = 'currentDay';
-							$content .= '<span class="today">Today</span>';
+							}
 						}
 
 						// Iterate over the event and see, which ones
 						// can be inserted into the calendar field.
 						foreach ($eventArray as $event) {
-
 							if ($event -> date()) {
-
 								// Publish event if date matches the one on calendar
 								if ($event -> date('Y-m-d') === sprintf($comparison, $i)) {
+									 			
+									 $eventClass = "";
 
-									$content .= '<p><a href="' . $event -> url() . '">' . html($event -> title()) . '</a></p>';
-									if ($event -> parent() -> uid() === 'events') {
-										$classes[] = 'ourEvent';
-									} elseif ($event -> parent() -> uid() === 'foreignevents') {
-										$classes[] = 'foreignEvent';
+								if( intval($event->date('j'),10) < intval($today['mday'],10) )
+									 {
+									 	$eventClass .= 'past';
+									 }
+								
+								   if ($event -> parent() -> uid() === 'events') 
+									{	
+										$eventClass .= 'ourEvent';
+									} else if ($event -> parent() -> uid() === 'foreignevents') {									
+										
+										$eventClass .= 'foreignEvent';
 									}
-
+								   
+									$content .= '<p class = "'.$eventClass.'">'.'<a href="' . $event -> url() . '">' . html($event -> title()) . '</a></p>';
 								}
 
 							}
