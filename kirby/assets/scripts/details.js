d9c6e2553c0de37525ac06502b8a51084092d180
Alexander Michael Roche <alex@amroche.co.uk>
2012-08-16 13:53:56 +0100

    changes to workshops, workshop signup and calendar

--- kirby/assets/scripts/details.js
+++ kirby/assets/scripts/details.js
@@ -1,77 +1,77 @@
-(function () {
-	"use strict";
-
-	var details_init, test_element, event_method;
-	
-	// Determine available event methods
-	test_element = document.createElement('details');
-	if (typeof test_element.addEventListener === "function") {
-		event_method = 0;
-	} else if (typeof test_element.attachEvent === "function") {
-		event_method = 1;
-	} else {
-		event_method = 2;
-	}
-	
-	details_init = function () {
-	
-		var i, event_func, details_collection;
-		
-		event_func = function (ev) {
-			ev.preventDefault();
-		
-			var i, details_element, sibling_details;
-			
-			// Verify that the target of the event is details/summary
-			if ((ev.target.tagName === 'SUMMARY') && (ev.target.parentNode.tagName === 'DETAILS')) {
-			
-				details_element = ev.target.parentNode;
-				if (details_element.hasAttribute('open')) {
-					details_element.removeAttribute('open');
-				} else {
-				
-					// Close everyone else
-					sibling_details = details_element.parentNode.getElementsByTagName('details');
-					for (i = 0; i < sibling_details.length; i++) {
-						sibling_details[i].removeAttribute('open');
-					}
-				
-					details_element.setAttribute('open', 'open');
-					
-				}
-			
-			}
-		
-		};
-
-		details_collection = document.getElementsByTagName('summary');
-		for (i = 0; i < details_collection.length; i++) {
-			if (details_collection[i].parentNode.tagName.toUpperCase() === 'DETAILS') {
-				if (event_method === 0) {
-					details_collection[i].addEventListener('click', event_func, false);
-				} else if (event_method === 1) {
-					details_collection[i].attachEvent('onclick', event_func);
-				} else {
-					details_collection[i].onload = event_func;
-				}
-			}
-		}
-	
-	};
-	
-	
-	if (event_method === 0) {
-		window.addEventListener('load', details_init, false);
-	} else if (event_method === 1) {
-		window.attachEvent('onload', details_init);
-	} else {
-		if (typeof window.onload === "function") {
-			test_element = window.onload;
-			window.onload = function () { test_element(); details_init(); }; 
-		} else {
-			window.onload = details_init;
-		}
-	}
-
-}());
-
+(function () {
+	"use strict";
+
+	var details_init, test_element, event_method;
+	
+	// Determine available event methods
+	test_element = document.createElement('details');
+	if (typeof test_element.addEventListener === "function") {
+		event_method = 0;
+	} else if (typeof test_element.attachEvent === "function") {
+		event_method = 1;
+	} else {
+		event_method = 2;
+	}
+	
+	details_init = function () {
+	
+		var i, event_func, details_collection;
+		
+		event_func = function (ev) {
+			ev.preventDefault();
+		
+			var i, details_element, sibling_details;
+			
+			// Verify that the target of the event is details/summary
+			if ((ev.target.tagName === 'SUMMARY') && (ev.target.parentNode.tagName === 'DETAILS')) {
+			
+				details_element = ev.target.parentNode;
+				if (details_element.hasAttribute('open')) {
+					details_element.removeAttribute('open');
+				} else {
+				
+					// Close everyone else
+					sibling_details = details_element.parentNode.getElementsByTagName('details');
+					for (i = 0; i < sibling_details.length; i++) {
+						sibling_details[i].removeAttribute('open');
+					}
+				
+					details_element.setAttribute('open', 'open');
+					
+				}
+			
+			}
+		
+		};
+
+		details_collection = document.getElementsByTagName('summary');
+		for (i = 0; i < details_collection.length; i++) {
+			if (details_collection[i].parentNode.tagName.toUpperCase() === 'DETAILS') {
+				if (event_method === 0) {
+					details_collection[i].addEventListener('click', event_func, false);
+				} else if (event_method === 1) {
+					details_collection[i].attachEvent('onclick', event_func);
+				} else {
+					details_collection[i].onload = event_func;
+				}
+			}
+		}
+	
+	};
+	
+	
+	if (event_method === 0) {
+		window.addEventListener('load', details_init, false);
+	} else if (event_method === 1) {
+		window.attachEvent('onload', details_init);
+	} else {
+		if (typeof window.onload === "function") {
+			test_element = window.onload;
+			window.onload = function () { test_element(); details_init(); }; 
+		} else {
+			window.onload = details_init;
+		}
+	}
+
+}());
+
--- kirby/assets/scripts/jobs.js
+++ kirby/assets/scripts/jobs.js
@@ -1,73 +1,73 @@
-(function () {
-	"use strict";
-	
-	var extender_init, test_element, event_method;
-	
-	// Determine available event methods
-	test_element = document.createElement('a');
-	if (typeof test_element.addEventListener === "function") {
-		event_method = 0;
-	} else if (typeof test_element.attachEvent === "function") {
-		event_method = 1;
-	} else {
-		event_method = 2;
-	}
-	
-	extender_init = function () {
-	
-		var event_func, extenders, i;
-		event_func = function (ev) {
-		
-			var divs, description_div;
-			ev.preventDefault();
-		
-			divs = this.parentNode.getElementsByTagName('div');
-			for (var i = 0; i < divs.length; i++) {
-			
-				if (divs[i].className.toLowerCase().indexOf('description') !== -1) {
-					description_div = divs[i];
-					break;
-				}
-			
-			}
-			if (typeof description_div !== 'undefined') {
-				if (description_div.className.indexOf('open') !== -1) {
-					description_div.className = description_div.className.replace(/\bopen\b/i, '').replace(/\s+/i, ' ').replace(/^\s+|\s+$/i, '');
-					this.innerHTML = 'View More';
-				} else {
-					description_div.className += ' open';
-					this.innerHTML = 'Hide';
-				}
-			}
-		
-		};
-		
-		extenders = document.getElementsByTagName('a');
-		for (i = 0; i < extenders.length; i++) {
-			if (extenders[i].className.toLowerCase().indexOf('extender') !== -1) {
-				if (event_method === 0) {
-					extenders[i].addEventListener('click', event_func, false);
-				} else if (event_method === 1) {
-					extenders[i].attachEvent('onclick', event_func);
-				} else {
-					extenders[i].onclick = event_func;
-				}
-			}
-		}
-	
-	};
-	
-	if (event_method === 0) {
-		window.addEventListener('load', extender_init, false);
-	} else if (event_method === 1) {
-		window.attachEvent('onload', extender_init);
-	} else {
-		if (typeof window.onload === 'function') {
-			test_element = window.onload;
-			window.onload = function () { test_element(); extender_init(); }; 
-		} else {
-			window.onload = extender_init;
-		}
-	}
-
+(function () {
+	"use strict";
+	
+	var extender_init, test_element, event_method;
+	
+	// Determine available event methods
+	test_element = document.createElement('a');
+	if (typeof test_element.addEventListener === "function") {
+		event_method = 0;
+	} else if (typeof test_element.attachEvent === "function") {
+		event_method = 1;
+	} else {
+		event_method = 2;
+	}
+	
+	extender_init = function () {
+	
+		var event_func, extenders, i;
+		event_func = function (ev) {
+		
+			var divs, description_div;
+			ev.preventDefault();
+		
+			divs = this.parentNode.getElementsByTagName('div');
+			for (var i = 0; i < divs.length; i++) {
+			
+				if (divs[i].className.toLowerCase().indexOf('description') !== -1) {
+					description_div = divs[i];
+					break;
+				}
+			
+			}
+			if (typeof description_div !== 'undefined') {
+				if (description_div.className.indexOf('open') !== -1) {
+					description_div.className = description_div.className.replace(/\bopen\b/i, '').replace(/\s+/i, ' ').replace(/^\s+|\s+$/i, '');
+					this.innerHTML = 'View More';
+				} else {
+					description_div.className += ' open';
+					this.innerHTML = 'Hide';
+				}
+			}
+		
+		};
+		
+		extenders = document.getElementsByTagName('a');
+		for (i = 0; i < extenders.length; i++) {
+			if (extenders[i].className.toLowerCase().indexOf('extender') !== -1) {
+				if (event_method === 0) {
+					extenders[i].addEventListener('click', event_func, false);
+				} else if (event_method === 1) {
+					extenders[i].attachEvent('onclick', event_func);
+				} else {
+					extenders[i].onclick = event_func;
+				}
+			}
+		}
+	
+	};
+	
+	if (event_method === 0) {
+		window.addEventListener('load', extender_init, false);
+	} else if (event_method === 1) {
+		window.attachEvent('onload', extender_init);
+	} else {
+		if (typeof window.onload === 'function') {
+			test_element = window.onload;
+			window.onload = function () { test_element(); extender_init(); }; 
+		} else {
+			window.onload = extender_init;
+		}
+	}
+
 }());
