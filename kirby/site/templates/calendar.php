0e65774edea16261a7c09c6f46c238fd4818519d
Alexander Michael Roche <alex@amroche.co.uk>
2012-08-29 15:05:56 +0100

    Apparently referencing tooltip.js in footer breaks it, but referencing it in markup without using snippet works fine.

--- kirby/site/templates/calendar.php
+++ kirby/site/templates/calendar.php
@@ -227,4 +227,64 @@ $offset = date("N", strtotime($currentMonthFormatted . '/01/' . $currentYear . '
 					?>			</div>
 			</div>		</section>
 	</div>
-<?php snippets('calendarFooter');?>
+		<footer>
+			<!--<?php echo kirbytext($site->copyright()) ?>-->
+		</footer>
+	</body>
+	<?php
+	if (isset($bottom_scripts))
+		foreach ($bottom_scripts as &$script)
+			echo('<script type="text/javascript" src="' . $script . '"></script>');
+	if (isset($bottom_snippets))
+		foreach ($bottom_snippets as &$snippet)
+			echo('<script type="text/javascript">' . $snippet . '</script>');
+?>
+
+<script src="assets/scripts/jQuery1-8-0.js"></script>
+<script src="assets/scripts/bootstrap-tooltip.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$("[rel=tooltip]").tooltip();
+	});
+	
+	$('#monthAdd').live('submit', function() { // catch the form's submit event
+	//event.preventDefault();
+	console.log($("#monthAdd :input"));
+	console.log($(this).attr('action')+$(this).serialize());
+    $.ajax({ // create an AJAX call...
+        data: $(this).serialize(), // get the form data
+        type: 'POST', // GET or POST
+        url: $(this).attr('action'),
+        success: function(response) { // on success..
+    //console.log(response);
+    //console.log(response);
+    var responder = $(response);
+    $('#calendarWrapper').html(responder.contents().find('#calendarWrapper').contents());
+    $("[rel=tooltip]").tooltip();
+        }
+    });
+    console.log("action stopped");
+    return false; // cancel original event to prevent form submitting
+});
+
+
+$('#monthSub').live('submit', function() { // catch the form's submit event
+	console.log($("#monthSub :input"));
+	console.log($(this).attr('action')+$(this).serialize());
+    $.ajax({ // create an AJAX call...
+        data: $(this).serialize(), // get the form data
+        type: 'POST', // GET or POST
+        url: $(this).attr('action'),
+        success: function(response) { // on success..
+    //console.log(response);
+    //console.log(response);
+    var responder = $(response);
+   $('#calendarWrapper').html(responder.contents().find('#calendarWrapper').contents());
+	$("[rel=tooltip]").tooltip();
+        }
+    });
+    console.log("action stopped");
+    return false; // cancel original event to prevent form submitting
+});
+</script>
+</html>
