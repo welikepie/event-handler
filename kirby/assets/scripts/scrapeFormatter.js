219043d11b6c543cd8cc8df1dbdf75395da27bd2
Alexander Michael Roche <alex@amroche.co.uk>
2012-09-12 14:13:10 +0100

    Added functionality to insert mock twitter avatar (waiting on final) in to space if not provided by Lanyrd for events.

--- kirby/assets/scripts/scrapeFormatter.js
+++ kirby/assets/scripts/scrapeFormatter.js
@@ -11,23 +11,36 @@ if(String(links[i]).indexOf("/profile") != -1){
 (function(){
 var organisers = document.getElementsByClassName("fullwidth");
 var returnArr = new Array(organisers.length);
-	for (i = 1; i < organisers.length; i= i + 2){
-	var replaceOrganisers = "";
-	var organiserList = organisers[i].getElementsByTagName("li");
-			//console.log("PAGENUM = "+i+"------------------------------------------------");
-			for(j = 0; j < organiserList.length; j ++){
-				var contains=false;
-				var innerList = organiserList[j].getElementsByTagName("span");
-				for(n=0;n < forbid.length; n++){
-					if(innerList[1].innerHTML.indexOf(forbid[n]) != -1){			
-						contains=true;
+	for (i = 0; i < organisers.length; i++){
+	if(i%2 == 0){
+		var replaceSpeakers = "";
+		var organiserList = organisers[i].getElementsByTagName("li");
+				for(j = 0; j < organiserList.length; j ++){
+					var innerList = organiserList[j].getElementsByClassName("avatar avatar-med");
+					for(k=0; k < innerList.length; k++){
+						if(innerList[k].innerHTML.length == 12){
+							innerList[k].innerHTML = "<img src = 'assets/images/spareAvatar.png'>";	//spareAvatar.png needs to be 40*40.
+						}
 					}
 				}
-				if(contains == false){
-					replaceOrganisers += "<li>"+organiserList[j].innerHTML+"</li>";				
-				}
 			}
-			organisers[i].innerHTML = "<ul class='people'>"+replaceOrganisers+"</ul>";		
-}
-
+	if(i%2 == 1){
+		var replaceOrganisers = "";
+		var organiserList = organisers[i].getElementsByTagName("li");
+				//console.log("PAGENUM = "+i+"------------------------------------------------");
+				for(j = 0; j < organiserList.length; j ++){
+					var contains=false;
+					var innerList = organiserList[j].getElementsByTagName("span");
+					for(n=0;n < forbid.length; n++){
+						if(innerList[1].innerHTML.indexOf(forbid[n]) != -1){			
+							contains=true;
+						}
+					}
+					if(contains == false){
+						replaceOrganisers += "<li>"+organiserList[j].innerHTML+"</li>";				
+					}
+				}
+				organisers[i].innerHTML = "<ul class='people'>"+replaceOrganisers+"</ul>";		
+			}
+	}
 })()
