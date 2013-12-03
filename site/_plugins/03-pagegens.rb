4d55e5dfb058720ba6bf0e65fd0a365ab0816ab2
Alexander Michael Roche <alex@amroche.co.uk>
2013-12-03 16:59:22 +0000

    Ordering in front page written with some parameters. Also picks a sub-template to display content depending on whether there is an event the next day or not.

--- site/_plugins/03-pagegens.rb
+++ site/_plugins/03-pagegens.rb
@@ -40,34 +40,18 @@ def generate_with_end(site,site_source,x_url, array)
 end
 
     def generate(site)
-#        lists_to_generate = ["events.yaml","speakers.yaml"];
-#        filepaths_to_write_to=["/events","/speakers"];
-#        template_to_use_arr=["defaulttest.html","defaulttest.html"];
-        #subfolders_to_check_for = ["meetups","other","workshops","conferences"];
         dir="site/_data/"
-#       //
-=begin
-        hash_of_data =  YAML.load_file(dir+"speakers.yaml");
-        end_dir = "/speakers"
-        template_to_use = "defaulttest.html"
-        hash_of_data.each {|x|
-        new_file_name = x[0]+".html"
-        site.pages << GenericPage.new(site, site.source, end_dir, new_file_name,template_to_use,x[1])
-        }
-=end
+
         foldersToTemplate = ["meetups","other","workshops"];
         subfolders_to_check_for = ["workshops"];
         hash_of_data =  YAML.load_file(dir+"events.yaml");
         hash_of_data.each {|x|
-  
-
         subfolders_to_check_for.each{|y|
         if(x["directory_tags"].include? y )
           if(x.has_key? "series")
               array = [];
               hash_of_data.each{|z| 
                 if(z["series"] == x["series"])
-                 # puts z["series"]
                   array << z;
                 end
               }
--- site/_plugins/04-liquidfilters.rb
+++ site/_plugins/04-liquidfilters.rb
@@ -1,31 +1,8 @@
-# Modulo filter for Jekyll
-#
-# Adds modulo functionality to Jekyll. It's already in the Liquid core, but that
-# version doesn't appear to be in Jekyll.
-#
-# That's about it.
- 
- require "Date"
-module Jekyll
-  module ModuloFilter
-    # Returns the modulo of the input based on the supplied modulus
-    # Called 'mod' to avoid conflict with newer Liquid's 'modulo' filter
-    def mod(input, modulus)
-    	puts input.to_i
-    	puts modulus.to_i
-    	puts       input.to_i % modulus.to_i    	
-      input.to_i % modulus.to_i    	
-    end
-  end
-end
-Liquid::Template.register_filter(Jekyll::ModuloFilter)
-
 module Jekyll 
-  module Month_listing
-  	def day_of_week (starting,ending)
+  module Calendar_functions
+  	def day_of_week (starting)
     	Date.civil( (date(starting,"%Y").to_i) , date(starting,"%m").to_i , date(starting,"%-d").to_i ).strftime("%u");
   	end
-
   	def add_days_return_unix (starting,ending)
     	Date.civil( (date(starting,"%Y").to_i) , date(starting,"%m").to_i , ending.to_i ).strftime("%s");
   	end
@@ -46,5 +23,105 @@ module Jekyll
     	Date.civil((date(x,"%Y").to_i),date(x,"%m").to_i, -1).strftime("%d");
     end
   end
+  module Frontpaging
+  	def within_x_days(starting, ending)
+  		starting.each{|x|
+  			if ((x["epoch_s_date"].to_i - Time.now().to_i) > 0 && (x["epoch_s_date"].to_i - Time.now().to_i) <(86400 * ending))
+  				#puts x["title"]
+  				return true;
+  			end
+  		}
+  		return false;
+  	end
+  	def sort_priorities(starting,ending)
+  		ending = ending.to_i;
+  		priorities = {"high"=>[],"medium"=>[],"low"=>[]}
+  		compiletime = Time.now().to_i;
+  		renderarr = []
+  		starting.each{ |x|
+  			x["seconds_difference_compile"] = difference(compiletime,time_since_epoch(x["date"]))
+  			if x.has_key?("priority")
+  				priorities[x["priority"].downcase] << x
+  			else
+  				priorities["medium"] << x
+  			end
+  			if !x.has_key?("size")
+  			x["size"]="medium"
+  			else
+  				x["size"] = x["size"].downcase
+  			end
+  		}
+  			priorities.each{ |x|
+	  				x[1].sort!{|y,z|
+	  					y["seconds_difference_compile"] <=> z["seconds_difference_compile"]
+	  				}
+  			}
+
+  			priorities.each{|x|
+  				x[1].each{ |y|
+  					renderarr << y
+  					if ending > 0
+  						if renderarr.length >= ending
+  							break
+  						end
+  					end
+  				}
+  			}
+  		starting = renderarr
+  	end
+  	def is_current(starting)
+  		arr = []
+  		starting.each{|x|
+  			if (x["epoch_s_date"].to_i - Time.now().to_i) > 0
+  				arr << x
+  			end
+  		}
+  		starting = arr
+  		return starting
+  	end
+
+  	def push(starting,ending)
+  		starting << ending
+  	end
+  	def merge(starting, ending)
+  		starting.concat(ending)
+  	end
+  end
 end 
-Liquid::Template.register_filter(Jekyll::Month_listing)
+
+def time_since_epoch(time)
+	begin
+		time=time.gsub("\s","T");
+		timeSplit=time.split("T");
+		if(timeSplit[0].split("-").length != 3)
+			puts "++ Something wrong with the YYYY-MM-DD portion of this date."
+			return 0;
+		end
+		if(timeSplit[1].split(":").length == 2 || timeSplit[1].split(":").length ==3)
+			if(timeSplit[1].split(":").length == 2)
+				time+=":00"
+			end
+		else
+			puts "++ Something wrong with the time portion of this date. Expected HH:MM or HH:MM:SS"
+			return 0;
+		end
+		return Time.iso8601(time).to_i;
+	rescue
+		return time.to_i
+	end
+end
+
+def difference(one, two)
+	if one > two
+		return one - two
+	end
+	if two > one
+		return two - one
+	end
+	if two == one
+		return 0
+	end
+end
+
+Liquid::Template.register_filter(Jekyll::Calendar_functions)
+Liquid::Template.register_filter(Jekyll::Frontpaging)
