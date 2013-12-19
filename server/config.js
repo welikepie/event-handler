459c79c44c8521b8df251f3bf312beb1e5fd6e89
Alex Roche <alexandermichaelroche@gmail.com>
2013-12-19 09:44:54 +0000

    Update config.js
    Bringing it in line with most recent commit to contain dummy functions and properties.

--- server/config.js
+++ server/config.js
@@ -1,5 +1,9 @@
 var mysql = require('mysql');
 module.exports = {
+	eventbrite: {
+		api:'', // Change this to your eventbrite API key
+		user:'' // Change this to your eventbrite USER key.
+	},
 	mailchimp: {
 		api:'',  // Change this to your mailchimp API Key
 		list:[''] // Change this to your mailchimp list ID
@@ -9,9 +13,11 @@ module.exports = {
 	"connection": mysql.createConnection({
 	  host:'',
 	  user:'',
+	  port:'',
 	  password:'',
 	  database:'',
 	  charset:'UTF8_GENERAL_CI'
 	})
-	}
+	},
+	verify:function(input){} //function used for verification of inputs from the user.
 };
