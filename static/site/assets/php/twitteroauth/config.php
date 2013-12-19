bc6d92293ebf3739593e4bb48dd52948437fa78c
Alexander Michael Roche <alex@amroche.co.uk>
2013-12-19 14:54:49 +0000

    Got Twitter login working quite happily to itself. Just need to merge code with templating properly.

---
+++ static/site/assets/php/twitteroauth/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', '');
+define('CONSUMER_SECRET', '');
+define('OAUTH_CALLBACK', 'http://127.0.0.1:65/event-handler/static/site/assets/php/twitteroauth-master/index.php');
+//define('OAUTH_CALLBACK', 'http://127.0.0.1:65/event-handler/static/site/assets/php/twitteroauth-master/index.php');
+
+$allowed_users = array('alexhacked','charlotteis','arranrp');
