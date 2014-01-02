2811c14cd1c96cccefe95b781e6fca3e1895bfc1
Alex Roche <alexandermichaelroche@gmail.com>
2014-01-02 10:02:04 +0000

    Update config.js

--- server/config.js
+++ server/config.js
@@ -3,6 +3,7 @@ module.exports = {
 	eventbrite: {
 		api:'', // Change this to your eventbrite API key
 		user:'' // Change this to your eventbrite USER key.
+		username:'' //Change this to your eventbrite account's email address.
 	},
 	mailchimp: {
 		api:'',  // Change this to your mailchimp API Key
